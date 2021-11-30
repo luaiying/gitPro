c894f9e564519c914d962fe98e6e3d10a1616ad4
luaiying <94438414+luaiying@users.noreply.github.com>
2021-11-30 07:48:02 +0000

    Update UserController.java

--- localGitpro/src/com/nnxy/controller/UserController.java
+++ localGitpro/src/com/nnxy/controller/UserController.java
@@ -1,5 +1,10 @@
 package com.nnxy.controller;
 
 public class UserController {
-
+ 
+   private int id;
+  private String username;
+  private String password;
+  private int sex;
+  private String love;
 }
